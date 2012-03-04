0f7f15b24df9f6d8b0545ced0369c4322907858f
Yuval Tobias <yuval@zot.co.il>
2012-03-04 15:03:19 +0200

    add services list and basic event#show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ FacebookImagesUpload::Application.routes.draw do
   resources :events do
     resources :photos
   end
+
+  resources :services
   
   match "auth/:provider/callback" => "services#create"
 end
