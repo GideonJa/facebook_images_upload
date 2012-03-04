81e9738e61f7d08bee6f397b8fa4edc6fb02cd8d
Yuval Tobias <yuval@zot.co.il>
2012-03-04 03:58:52 +0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookImagesUpload::Application.config.secret_token = '9033e6fb39848654eeb34c6cff3b68711d1016618a5da5b6e4ed481711bcef3c181ba7bb4a8c852352b9b33cdb621be2c4ab07b08bbd4ace4e825cbb0773b408'
