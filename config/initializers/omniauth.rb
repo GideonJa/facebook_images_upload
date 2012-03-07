5f48bc524bcfc6b287eb4b5a94a5a4a29f75045d
Yuval Tobias <yuval@zot.co.il>
2012-03-07 09:40:00 +0200

    add Facebook-specific OmniAuth configuration.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  Provider.all.each do |p|
-    provider p.name.to_sym, p.consumer_key, p.consumer_secret
-  end
+  # Provider.all.each do |p|
+  #   provider p.name.to_sym, p.consumer_key, p.consumer_secret
+  # end
+  facebook = Provider.find_by_name('facebook')
+  provider :facebook, facebook.consumer_key, facebook.consumer_secret, {:client_options => {:ssl => {:ca_file => "/etc/ssl/certs/ca-bundle.crt"}}, :scope => 'user_photos'}
+
 end
