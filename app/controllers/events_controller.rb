a0707278dee928ee3ac7319ca1e45de565da4850
Yuval Tobias <yuval@zot.co.il>
2012-03-07 10:02:44 +0200

    add placeholder for Koala api

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,13 @@
 class EventsController < ApplicationController
 
   def show
-    puts current_user.events.count
     @event = current_user.events.find(params[:id])
+    
+    facebook_service = current_user.services.find_by_provider_id(Provider.find_by_name('facebook').id)
+    if facebook_service # and showing the uncategorized event?
+      api = Koala::Facebook::API.new(facebook_service.oauth_token)
+      # TODO: go over every album, in each album download every photo and add it to the uncategorized event
+    end
   end
   
 end
