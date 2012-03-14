32b4ff9d0f441dced957a2309fa767191490be19
Yuval Tobias <yuval@zot.co.il>
2012-03-14 20:33:58 +0200

    add ability to automatically convert metadata to Hash

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
+  serialize :metadata, Hash
   belongs_to :event
   belongs_to :service
   has_attached_file :file, :styles => { :medium => "300x300>", :thumb => "100x100>" }
