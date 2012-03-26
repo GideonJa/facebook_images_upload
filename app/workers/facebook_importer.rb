9b14b2c4fe2551860325a119293662baf8e07c44
Yuval Tobias <yuval@zot.co.il>
2012-03-26 13:32:54 +0200

    count the total number of photos that will be imported before downloading them, and update the number of photos imported as they get imported.

--- app/workers/facebook_importer.rb
+++ app/workers/facebook_importer.rb
@@ -8,6 +8,18 @@ class FacebookImporter
     service.update_attribute :import_started_at, Time.now
     user = User.find(service.user_id)
     api = Koala::Facebook::API.new(service.oauth_token)
+
+    total_photos = 0
+    api.get_connections('me', 'albums').each do |album|
+      api.get_connections(album['id'], 'photos').each do |photo|
+        unless user.photos.find_by_provider_uid(photo['id'])
+          total_photos += 1
+        end
+      end
+    end
+    service.update_attributes :import_total_photos => total_photos, :import_num_photos => 0
+    
+    num_photos = 0
     api.get_connections('me', 'albums').each do |album|
       api.get_connections(album['id'], 'photos').each do |photo|
         unless user.photos.find_by_provider_uid(photo['id'])
@@ -17,6 +29,8 @@ class FacebookImporter
             :provider_uid => photo['id'],
             :metadata => {'album' => album, 'photo' => photo},
             :file => open(photo['source']) )
+          num_photos += 1
+          service.update_attribute :import_num_photos, num_photos
         end
       end
     end    
